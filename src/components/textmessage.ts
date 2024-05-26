d112bd7b8c3ad2c56da0a86a4ab46b18c10445f1
lewla <git@lew.la>
2024-05-26 16:28:06 +0100

    change message styling

--- src/components/textmessage.ts
+++ src/components/textmessage.ts
@@ -21,19 +21,29 @@ templateElement.innerHTML = /* HTML */`
             font-weight: 400;
         }
         .avatar {
-            max-width: 32px;
             border-radius: 100%;
+            width: 32px;
+        }
+        .timestamp {
+            font-size: 12px;
+            color: var(--lightergray);
+            margin-left: 10px;
         }
         .left-section {
             user-select: none;
         }
+        .metadata {
+            align-items: center;
+            line-height: 12px;
+        }
         .main-section {
             display: flex;
             flex-direction: column;
-            gap: 3px;
+            gap: 5px;
             overflow: hidden;
             overflow-wrap: break-word;
             flex-grow: 1;
+            position: relative;
         }
     </style>
     <div class="message" data-id="">
@@ -41,7 +51,9 @@ templateElement.innerHTML = /* HTML */`
             <img src="resources/images/256.png" class="avatar">
         </div>
         <div class="main-section">
-            <span class="member"></span>
+            <span class="metadata">
+                <span class="member"></span><span class="timestamp"></span>
+            </span>
             <span class="body"></span>
         </div>
     </div>
@@ -61,6 +73,7 @@ export class TextMessageElement extends BaseElement {
         const messageEl = this.shadowRoot?.querySelector('.message')
         const bodyEl = this.shadowRoot?.querySelector('.body')
         const memberEl = this.shadowRoot?.querySelector('.member')
+        const timestampEl = this.shadowRoot?.querySelector('.timestamp')
 
         if (messageEl instanceof HTMLElement) {
             messageEl.dataset.id = this.content.id
@@ -75,6 +88,10 @@ export class TextMessageElement extends BaseElement {
                 memberEl.textContent = app.members.get(this.content.member.id)?.display_name ?? ''
             }
             memberEl.dataset.id = this.content.member?.id ?? memberEl.dataset.id
+        }
+
+        if (timestampEl instanceof HTMLElement) {
+            timestampEl.textContent = new Date().toISOString()
         }
     }
 
